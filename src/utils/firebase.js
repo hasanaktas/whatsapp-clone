c44960abf9efbb5b422df3a04151b75e7245caf1
Hasan Aktas <hasanaktas95@gmail.com>
2021-10-10 14:27:50 +0300

    first commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBy6U78iuXIm1B2I54PG1-y4djgrYVL0x4",
+  authDomain: "whatsapp-clone-fbc59.firebaseapp.com",
+  projectId: "whatsapp-clone-fbc59",
+  storageBucket: "whatsapp-clone-fbc59.appspot.com",
+  messagingSenderId: "562332764910",
+  appId: "1:562332764910:web:f31ab2abb3f7aad234c1ff",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
